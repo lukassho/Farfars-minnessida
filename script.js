0e7189ca4d077ac530f5dbbb5927a1e6825673aa
lukassho <110125953+lukassho@users.noreply.github.com>
2025-03-06 13:58:47 +0100

    .

--- script.js
+++ script.js
@@ -81,21 +81,17 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (window.location.pathname.includes("poul.html")) { 
+        const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 450) { 
-            scrollToTopBtn.classList.add("show");
-        } else {
-            scrollToTopBtn.classList.remove("show");
-        }
-    });
-
-    scrollToTopBtn.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 450) { 
+                scrollToTopBtn.classList.add("show");
+            } else {
+                scrollToTopBtn.classList.remove("show");
+            }
         });
-    });
+    }
 });
+
 
